11d6954dd7c39ebe494911c925c812d0a29f904a
Annavarapu Jashwanth <jashwanthannavarapu99@gmail.com>
2026-03-17 02:10:03 +0530

    Fix undefined phoneInputModal reference - remove empty conditional block

--- frontend/src/components/Tasks.jsx
+++ frontend/src/components/Tasks.jsx
@@ -499,12 +499,6 @@ ${photoUrl}`;
         </div>
       )}
 
-      {/* Phone Input Modal - for Task Assignment */}
-      {phoneInputModal && (
-        <></>
-      )}
-
-
       {/* Message Preview & Edit Modal */}
       {showMessageModal && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
